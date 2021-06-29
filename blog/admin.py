e7bba8227dfde0460419d92c7c0d6ed0c1ad45e5
unknown <pravinsvader@gmail.com>
2021-06-29 14:30:16 +0530

    first commit

---
+++ blog/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+
+# Register your models here.
+from .models import Post
+
+@admin.register(Post)
+class PostModelAdmin(admin.ModelAdmin):
+    list_display = ['id','title', 'desc']
